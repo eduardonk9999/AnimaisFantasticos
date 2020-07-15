9295e43fa350e28f10d59f99c5fbfd892efa503f
Eduardo <thiago.novak7@gmail.com>
2020-07-15 09:46:13 -0300

    Add, accordion, js and css

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo')
+//Navegação por tab
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-    function activeTabe(index) {
-        tabContent.forEach(function(itemContent){
-            itemContent.classList.remove('ativo');
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo')
+
+        function activeTabe(index) {
+            tabContent.forEach(function (itemContent) {
+                itemContent.classList.remove('ativo');
+            });
+
+            tabContent[index].classList.add('ativo');
+        };
+
+        tabMenu.forEach(function (itemMenu, index) {
+            itemMenu.addEventListener('click', function () {
+                activeTabe(index);
+            })
         });
-
-        tabContent[index].classList.add('ativo');
     };
 
-    tabMenu.forEach(function(itemMenu, index){
-        itemMenu.addEventListener('click', function(){
-            activeTabe(index);
-        })
-    });
 }
+initTabNav();
+
+//Acordion List
+function innitAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+
+    accordionList[0].nextElementSibling.classList.add('ativo');
+
+    if (accordionList.length) {
+        function activeAccordion() {
+            accordionList.forEach(function (itemAccordion) {
+                itemAccordion.nextElementSibling.classList.remove('ativo');
+                itemAccordion.classList.remove('ativo');
+            });
+
+            this.nextElementSibling.classList.toggle('ativo');
+            this.classList.toggle('ativo');
+        }
+
+        accordionList.forEach(function (item) {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+};
+innitAccordion();
